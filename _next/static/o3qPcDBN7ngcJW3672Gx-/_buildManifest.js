7e34c3e71c31d4925f6082250d35d18fa52d6a80
YoTeTe <50970697+YoTeTe@users.noreply.github.com>
2021-09-05 08:42:21 +0000

    Deploying to gh-pages from @ YetAnotherFriday/YetAnotherFriday.github.io@6ef5a9511a1e251406da7eddd5ce202902f78d20 🚀

--- _next/static/o3qPcDBN7ngcJW3672Gx-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6627650d92a7f6a9a811.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],"/[article]":["static/chunks/pages/[article]-6e9181224d87e547f577.js"],sortedPages:["/","/_app","/_error","/[article]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
